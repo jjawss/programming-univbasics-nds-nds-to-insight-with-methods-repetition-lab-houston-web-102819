e94b09535fd09961c74cceb9e59df83919221e5a
David Jaworski <jaws360@gmail.com>
2019-10-25 19:02:11 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -52,10 +52,10 @@ def total_gross(source)
   
   list_of_directors[source]
   #returns an array of director names
-  i = 0
-  while i < list_of_directors[source]
-    total += [list_of_directors](source)[i]directors_totals(source)
-    i += 1
+  #i = 0
+  #while i < list_of_directors[source]
+    #total += [list_of_directors](source)[i]directors_totals(source)
+    #i += 1
   
 end
 
